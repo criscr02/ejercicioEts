9b4be15f9917e7f6cbc60fc2d21818390f4fc58e
Cristian Cantelar Rodriguez <cristiancantear02@gmail.com>
2022-03-16 17:13:46 +0000

    Subido el proyecto con todo lo requerido, y la clase Alumno dentro del modulo area

---
+++ ejercicioEts/area/src/cristiancantelarrodriguez.java
@@ -0,0 +1,29 @@
+public class cristiancantelarrodriguez {
+    String tipo;
+    Integer valor1;
+    Integer valor2;
+
+
+    //Constructor por defecto
+    public cristiancantelarrodriguez(){
+
+    }      
+    //Constructor con un parámetro.
+    public cristiancantelarrodriguez(Integer valor1){
+        this.valor1=valor1;
+    }
+
+    //Constructor con dos parámetros.
+    public cristiancantelarrodriguez(Integer valor1, Integer valor2){
+        this.valor1=valor1;
+        this.valor2=valor2;
+    }
+
+    /** Metodo para calcular el area mediante dos paramateros dados por el usuario.
+     * 
+     */
+   public Integer calculoArea(Integer valor1, Integer valor2){
+
+   }
+
+}
